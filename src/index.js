4467de4e0f07d7ec9886d2ef79e1e200733512a4
Frank Küster <kuester@de.ibm.com>
2022-11-28 16:37:28 +0100

    Lesson 157: Broadcasting events

--- src/index.js
+++ src/index.js
@@ -14,11 +14,23 @@ app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
-
+    // socket.emit sends a message to a particular connection, for example, when a new user 
+    // connects.
     socket.emit('message', 'Welcome!')
+    // broadcast emit sends the message to all participants, except the one that the 
+    // socket connection is referring to.
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message) => {
+        // io.emits sends a message to all connections (all participants)
         io.emit('message', message)
+    })
+    // disconnect is another built-in event, like connection. It does not have to be defined.
+    // We want to send a message when a participant leaves the chat. We don't have to use broadcast
+    // because the user has already disconnected when this function runs; io.emit will send the 
+    // message to the remaining participants.
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
     })
 })
 server.listen(port, () => {

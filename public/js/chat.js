f82c220a3419fc68a13528cd2d52fdc860495152
Frank Küster <kuester@de.ibm.com>
2022-11-30 15:54:35 +0100

    Lesson 160: Form and Button States

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,12 @@
 const socket = io()
 
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+// $ in front of the variable name is just a convention. It signifies 
+// that we're referring to a DOM element.
+const $sendLocationButton = document.querySelector('#send-location')
+
 // server (emit) => client (receive) --acknowledgement --> server
 // client (emit) => server (receive) --acknowledgement --> client
 
@@ -7,15 +14,27 @@ socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
     // when we use the submit event, we have access to the 'e' (event) object.
+
+    // Disabling the form once it's been submitted:
+    $messageFormButton.setAttribute('disabled', 'disabled')
+    // DOM manipulation. There is a 'disabled' attribute on a button, and one of its possible values is 
+    // also 'disabled'.
+    // Clearing the messsage text input field:
+    $messageFormInput.value = ''
+    //Putting the cursor focus back on the message input field:
+    $messageFormInput.focus()
     const message = e.target.elements.message.value
     // e.target ... this is less error-prone than using document.querySelector
     // because the element IDs might change, or the target element is no longer 
     // the first (only) element of its type.
 
     socket.emit('sendMessage', message, (error) => {
+        // Re-enabling the messsage button:
+        $messageFormButton.removeAttribute('disabled')
+
         if (error) {
             return console.log(error)
         }
@@ -23,16 +42,21 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     })
 })
 // Button that discloses one's location to the other participants
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Your browser does not support geolocation!')
     }
-    // getCurrentPosition is asynchronous, but does not support the async/await syntax.
-    // That's why we use a callback function.
+   
+    // Disabling the button just before we send the location:
+    $sendLocationButton.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition((position) => {
+        // getCurrentPosition is asynchronous, but does not support the async/await syntax.
+        // That's why we use a callback function.
         // position is an object returned by the function.
         const pos = { latitude: position.coords.latitude, longitude: position.coords.longitude }
         socket.emit('sendLocation', pos, () => {
+            // Re-enabling the Send Location button:
+            $sendLocationButton.removeAttribute('disabled')
             console.log('Location shared.')
         })
     })

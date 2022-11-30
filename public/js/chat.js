ad3f4ad24003aaecb3927813c93e723afb8b6af9
Frank Küster <kuester@de.ibm.com>
2022-11-30 16:42:36 +0100

    Lesson 161: Rendering Messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,12 +6,27 @@ const $messageFormButton = $messageForm.querySelector('button')
 // $ in front of the variable name is just a convention. It signifies 
 // that we're referring to a DOM element.
 const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+// Templates:
+const messageTemplate = document.querySelector('#message-template').innerHTML
+// innerHTML: we want the div inside the template
 
 // server (emit) => client (receive) --acknowledgement --> server
 // client (emit) => server (receive) --acknowledgement --> client
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+        // shorthand for 'message: message'
+        // What we pass in here as the value of the message key is the message that this function 
+        // works on, allowing us to render different (dynamic) content to the HTML user interface.
+    })
+    // Inserting the template content in the messages div:
+    $messages.insertAdjacentHTML('beforeend', html)
+    // 'beforeend' one of the possible options, which means before the closing </div> tag.
+
 })
 
 $messageForm.addEventListener('submit', (e) => {
@@ -22,15 +37,16 @@ $messageForm.addEventListener('submit', (e) => {
     $messageFormButton.setAttribute('disabled', 'disabled')
     // DOM manipulation. There is a 'disabled' attribute on a button, and one of its possible values is 
     // also 'disabled'.
-    // Clearing the messsage text input field:
-    $messageFormInput.value = ''
-    //Putting the cursor focus back on the message input field:
-    $messageFormInput.focus()
+    
     const message = e.target.elements.message.value
     // e.target ... this is less error-prone than using document.querySelector
     // because the element IDs might change, or the target element is no longer 
     // the first (only) element of its type.
-
+    
+    // Clearing the messsage text input field:
+    $messageFormInput.value = ''
+    //Putting the cursor focus back on the message input field:
+    $messageFormInput.focus()
     socket.emit('sendMessage', message, (error) => {
         // Re-enabling the messsage button:
         $messageFormButton.removeAttribute('disabled')
